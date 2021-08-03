03a9adaba2a5ea7d1ea50be7feeba9d98fa8d51f
katherin serna meza <katherin.serna@tectijuana.edu.mx>
2021-08-02 17:31:32 -0700

    Assets, Metadatos, CSS, layout component, Estilos globales

--- pages/posts/second-post.js
+++ pages/posts/second-post.js
@@ -1,14 +1,16 @@
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function SecondPost() {
     return (
-        <>
+        <Layout>
             <h1>Second Post</h1>
             <h2>
                 <Link href="/posts/first-post">
                     <a>Back to the future</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
+
     )
 }
